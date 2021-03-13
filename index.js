0f64318e57a29571aba601a02b19f88f421ead8b
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-03-13 23:10:06 +0900

    small update

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ Client.prototype.registerCommandToGuild = function (guildId, data) {
 	return this.api.applications(this.user.id).guilds(guildId).commands.post({data});
 }
 
+/**
+ * 
+ * @param {Discord.Client} client 
+ */
 module.exports = function (client) {
 	client.ws.on('INTERACTION_CREATE', async interaction => {
-		client.emit('interactionCreate', interaction);
+		client.channels.fetch(interaction.channel_id).then(channel => {
+			interaction.member = new Discord.GuildMember(client, interaction.member, channel.guild);
+			interaction = channel;
+			client.emit('interactionCreate', interaction);
+		});
 	});
 }
