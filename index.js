e19f07110134cde1d46c1998ba14969f63b6d5ae
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-03-14 11:28:55 +0900

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const Client = Discord.Client;
-const handlers = require('discord.js').WebSocketManager
+const Constants = require('./Constants');
 
 Client.prototype.registerCommand = function (data) {
 	return this.api.applications(this.user.id).commands.post({data});
@@ -16,9 +16,17 @@ Client.prototype.registerCommandToGuild = function (guildId, data) {
  */
 module.exports = function (client) {
 	client.ws.on('INTERACTION_CREATE', async interaction => {
-		client.channels.fetch(interaction.channel_id).then(channel => {
+		client.channels.fetch(interaction.channel_id).then(async channel => {
 			interaction.member = new Discord.GuildMember(client, interaction.member, channel.guild);
 			interaction.channel = channel;
+			if (interaction.data.options) {
+				for (const i in interaction.data.options) {
+					const option = interaction.data.options[i];
+					if (option.type === Constants.USER) option.value = await client.users.fetch(option.value);
+					else if (option.type === Constants.CHANNEL) option.value = await client.channels.fetch(option.value);
+					else if (option.type === Constants.ROLE) option.value = await channel.guild.roles.fetch(option.value);
+				}
+			}
 			client.emit('interactionCreate', interaction);
 		});
 	});
